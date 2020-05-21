2f18b8503757f1428df101cefc4e7c772d3d7b19
Abdul_Omar <amomar@ucsd.edu>
2020-05-20 18:38:32 -0700

    Updates

---
+++ precache-manifest.1bb2ffdf1bd763757f5aee0b1e789a31.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40552c7ea55f2bb0ad5e2d6416c328af",
+    "url": "/charityadvisor/index.html"
+  },
+  {
+    "revision": "ddf522f1e2ac2717635d",
+    "url": "/charityadvisor/static/css/2.dcd209ce.chunk.css"
+  },
+  {
+    "revision": "e2bff7da377d71da522b",
+    "url": "/charityadvisor/static/css/main.9829d7ba.chunk.css"
+  },
+  {
+    "revision": "ddf522f1e2ac2717635d",
+    "url": "/charityadvisor/static/js/2.6f0005e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/charityadvisor/static/js/2.6f0005e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2bff7da377d71da522b",
+    "url": "/charityadvisor/static/js/main.51ddbf6c.chunk.js"
+  },
+  {
+    "revision": "610e7ff0709c82c07167",
+    "url": "/charityadvisor/static/js/runtime-main.1641b73b.js"
+  },
+  {
+    "revision": "0d34e80661d8b954eaac005eba8dc067",
+    "url": "/charityadvisor/static/media/background.0d34e806.jpeg"
+  },
+  {
+    "revision": "e3893f9b38dfa350115d568bee0261fc",
+    "url": "/charityadvisor/static/media/background2.e3893f9b.jpg"
+  },
+  {
+    "revision": "1febad352ce9365f17633e66521f4cc3",
+    "url": "/charityadvisor/static/media/background4.1febad35.jpg"
+  },
+  {
+    "revision": "4256c48a62bf1e52e57df814de6e4892",
+    "url": "/charityadvisor/static/media/background5.4256c48a.jpg"
+  }
+]);
