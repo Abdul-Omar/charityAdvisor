705a1e902694e8f36e172f25b6995f39dafde205
Abdul_Omar <amomar@ucsd.edu>
2020-06-07 00:59:37 -0700

    minor changes to ui

--- src/App.js
+++ src/App.js
@@ -54,32 +54,32 @@ class App extends Component {
               <Navbar onRouteChange= { this.onRouteChange}> </Navbar> 
 
               {route === "Home"? 
-              <div className="flex"> 
-                <div className="center mt7 w-30 pa0">
-                   <h1 className="light-red dark-green orange fw6">  Let's fight food waste together and help those in need in the process.</h1>
-                  <h3 className="light-red fw3 dark-green orange">
-                      Every day, thousands of pounds of perfectly good food 
-                      is wasted in the hotel/Restaurant industry while
-                      thousands of needy people go hungry.
-                      We want to solve this problem by
-                      giving you a chance to connect instantly with 
-                      charities nearby who could 
-                      distribute this food to deserving people!
-                  </h3>
+                  <div className=" flex flex-wrap"> 
+                    <div className="center mt7 w-30 pa0">
+                       <h1 className="light-red dark-green orange fw6">  Let's fight food waste together and help those in need in the process.</h1>
+                      <h3 className="light-red fw3 dark-green orange">
+                          Every day, thousands of pounds of perfectly good food 
+                          is wasted in the hotel/Restaurant industry while
+                          thousands of needy people go hungry.
+                          We want to solve this problem by
+                          giving you a chance to connect instantly with 
+                          charities nearby who could 
+                          distribute this food to deserving people!
+                      </h3>
 
-                  <button className="link  h-20 grow  w-25 white f6 pointer dim bg-dark-green br-pill"  onClick={() => this.onRouteChange('Search')}  >  Let's Go! </button>
+                      <button className="link  h-20 grow  w-20 white f6 pointer dim bg-dark-green br-pill"  onClick={() => this.onRouteChange('Search')}  >  Let's Go! </button>
+                  </div>
+
+                  <img className=" o-75 flex h5 center mt7" src={food}/>
+              </div>
+              :
+              <div className ="center mt7 w-300" >
+                 <SearchBar getData={this.getDataFromSearchBar}/>
+                <CharityList data={this.state.list}/>
               </div>
 
-              <img className=" flex h5 center mt7" src={food}/>
-          </div>
-          :
-          <div className ="center mt7 w-300" >
-             <SearchBar getData={this.getDataFromSearchBar}/>
-            <CharityList data={this.state.list}/>
-          </div>
-
-      } 
-      </div>
+            } 
+        </div>
     );
   }
 }
