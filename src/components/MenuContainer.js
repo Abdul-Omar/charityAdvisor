f229a1031a1ce22d5f763d25c61e845491460026
Abdul_Omar <amomar@ucsd.edu>
2020-06-19 15:57:17 -0700

    fixed slider not closing on click bug

--- src/components/MenuContainer.js
+++ src/components/MenuContainer.js
@@ -5,17 +5,44 @@ import './Menu.css';
 import 'tachyons';
 
 
-const MenuContainer  =  ({onRouteChange}) => {
+class MenuContainer  extends React.Component {
+
+   constructor(props){
+
+    super(props);
+    this.state = {
+      menuOpen: false
+    }
+  }
+
+    // This keeps your state in sync with the opening/closing of the menu
+    // via the default means, e.g. clicking the X, pressing the ESC key etc.
+    handleStateChange (state) {
+      this.setState({menuOpen: state.isOpen});
+    }
+    
+    // This can be used to close the menu, e.g. when a user clicks a menu item
+    closeMenu () {
+      this.setState({menuOpen: false});
+    }
 
 
-    return (
-      
-      <Menu> 
-        <a  className="menu-item white link pointer" onClick={() => onRouteChange('Home')}  >Home</a>
-        <a  className="menu-item white link pointer" onClick={() => onRouteChange('Search')}  >Search</a>
-      </Menu>
+    render(){
 
-    );
+        const{onRouteChange} = this.props;
+        return (
+          
+          <Menu
+
+              isOpen={this.state.menuOpen}
+              onStateChange={(state) => this.handleStateChange(state)}
+          > 
+            <a  className="menu-item white link pointer" onClick={() => {onRouteChange('Home'); this.closeMenu();}} >Home</a>
+            <a  className="menu-item white link pointer" onClick={() => {onRouteChange('Search'); this.closeMenu();}}>Search</a>
+          </Menu>
+
+        );
+    }
 }
 
 export default MenuContainer;
